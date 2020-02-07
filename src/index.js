86bac1e00522c3c24a317deaa2512b812144c58f
averagealloy <m.mctighe10@gmail.com>
2020-02-07 14:54:26 -0500

    removed a functional component and added a class comp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+class App extends React.Component {
+    render() {
 
-const App = () => {
-    return <div>Hi there!</div>;
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Latitude:</div>
+    }
 };
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App/>, document.querySelector('#root'));
